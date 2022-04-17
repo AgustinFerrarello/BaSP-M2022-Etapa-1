d46c2b5ec018dade8b44f77d9968c774928a5715
Agustín <ferrarello.agustin@gmail.com>
2022-04-17 12:03:16 -0300

    BaSP-2022: ex01-variables and operators

---
+++ semana-5/exercises/01_variables_and_operators.js
@@ -0,0 +1,17 @@
+//exercice 1.a.
+var valueA = 20;
+var valueB = 50;
+var sumA = valueA + valueB;
+console.log("Ex-a:", "Result A+B =", sumA);
+
+//exercise 1.b.
+var valueC = 'Hello';
+var valueD = 'Word';
+var result = valueC + valueD;
+console.log("Ex-b:", result)
+
+//exercise 1.c.
+var valueE = "four";
+var valueF = "six";
+var sumLength = valueE.length + valueF.length
+console.log("Ex-c:", "Quantity of letters", sumLength)
